239000e487854801f49482fb04252b29e45e76d4
Jorge <41489616+JorgeDLM@users.noreply.github.com>
2025-09-13 15:52:11 -0600

    intento de rebuildear con favicon y icono

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -29,11 +29,12 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
           <div className="flex items-center gap-3">
             <a href="/" className="focus:outline-none flex items-center gap-3">
               <Image 
-                src="/icono-welovecode.webp" 
+                src="/icono-welovecode.png" 
                 alt="WeLoveCode Logo" 
                 width={50} 
                 height={50} 
                 className="rounded-lg"
+                priority
               />
               <div className="text-2xl font-black bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
                 WeLoveCode
